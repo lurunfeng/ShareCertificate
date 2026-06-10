0030c24838fc3f9f34ce26ce19f0d5db4e46f1f3
lurunfeng <1145646893@qq.com>
2026-06-10 11:49:50 +0800

    格式化1

--- Stocks.py
+++ Stocks.py
@@ -1,33 +1,29 @@
 import efinance as ef
 import time
 import os
+from tabulate import tabulate
 
-my_codes = ["300323",
-            "600740",
-            "000751",
-            "601990",
-            "600059",
-            "000019",
-            "601179",
-            "603258",
-            "600422",
-            "600879",
-            "601066",
-            "002716",
-            "300589",
-            "300129"
-            ]
+my_codes = [
+    "300323", "600740", "000751", "601990",
+    "600059", "000019", "601179", "603258",
+    "600422", "600879", "601066", "002716",
+    "300589", "300129"
+]
 
 while True:
     os.system("cls")
     try:
-        # 拿全市场
         df = ef.stock.get_realtime_quotes()
-        # 过滤你要的
         df = df[df["股票代码"].isin(my_codes)]
         df = df[["股票代码", "股票名称", "最新价", "涨跌幅"]]
-        print("=" * 50)
-        print(df.to_string(index=False))
+
+        # 转为列表格式，适配 tabulate
+        table_data = df.values.tolist()
+        headers = df.columns.tolist()
+
+        # 输出网格表格（支持 plain/simple/grid/markdown 等样式）
+        print(tabulate(table_data, headers=headers, tablefmt="grid", stralign="left", numalign="right"))
+
     except Exception as e:
         print("异常：", e)
-    time.sleep(5)
+    time.sleep(60)
